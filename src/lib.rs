f994f397197f303a240a58ee1ae8c649c187e7d5
Evan Stoll <evanjsx@gmail.com>
2018-10-22 19:49:43 -0400

    add missing files

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,87 @@
+pub mod posts;
+
+extern crate reqwest;
+
+#[macro_use]
+extern crate serde_derive;
+
+use std::collections::HashMap;
+
+extern crate more_asserts;
+extern crate core;
+
+use image::*;
+//fn get_posts(limit: u32, page: u32, random: u32, raw: u32) -> posts::RootInterface {
+//    let mut map = HashMap::new();
+//    map.insert("limit", limit);
+//    map.insert("page", page);
+//    map.insert("random", random);
+//    map.insert("raw", raw);
+//
+//    let url = reqwest::Url::parse_with_params("https://danbooru.donmai.us/posts.json", &map);
+//
+//    let response = reqwest::get(url);
+//    let text = response.unwrap().text().unwrap();
+//    let posts: posts::RootInterface = serde_json::from_str(&text).unwrap();
+//    posts
+//}
+
+fn get_posts() -> Vec<posts::RootInterface> {
+    let url = reqwest::Url::parse("https://danbooru.donmai.us/posts.json").ok().unwrap();
+
+    let response = reqwest::get(url).unwrap().text().unwrap().to_string();
+    let posts: Vec<posts::RootInterface> = serde_json::from_str(&response).unwrap();
+    posts
+}
+
+pub fn search_tag(query: String) -> Vec<posts::RootInterface> {
+    let url = reqwest::Url::parse(format!("https://danbooru.donmai.us/posts.json?limit=100&tags={}", query).as_str()).ok().unwrap();
+    let response = reqwest::get(url).unwrap().text().unwrap().to_string();
+    let posts: Vec<posts::RootInterface> = serde_json::from_str(&response).unwrap();
+    posts
+}
+
+// todo: most of these functions accomplish half of the same things, ie basic get request -> Post list.  try to move url parsing and response handling out
+pub fn get_post_by_id(id: u64) -> posts::RootInterface {
+    let url = reqwest::Url::parse(format!("https://danbooru.donmai.us/posts/{}.json", id).as_str()).ok().unwrap();
+//    println!("Going to try to get response from url {}", url.to_string());
+    let response = reqwest::get(url).unwrap().text().unwrap().to_string();
+    let post: posts::RootInterface = serde_json::from_str(&response).unwrap();
+    post
+}
+
+fn get_image(post: posts::RootInterface) -> DynamicImage {
+    let url = reqwest::Url::parse(&post.get_file_url()).ok().unwrap();
+    let mut buf: Vec<u8> = vec![];
+    let h_img = reqwest::get(url).unwrap().copy_to(&mut buf);
+    let img = image::load_from_memory(&buf);
+    img.unwrap()
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn null_fields() {
+        let data = get_posts();
+        assert_gt!(data.len(), 0);
+        data.iter().filter(|p| p.get_last_comment_bumped_at() == "").for_each(|x| println!("{:?}", x));
     }
+
+    #[test]
+    fn get_image() {
+        let u = "https://danbooru.donmai.us/data/sample/sample-e976cde6cc68aca3108527561798b980.jpg";
+        let post = get_post_by_id("3293386".to_string());
+        let img = super::get_image(post);
+        assert_eq!(img.width(), 1032);
+        assert_eq!(img.height(), 838);
+    }
+
+    #[test]
+    fn get_post() {
+        let post = get_post_by_id("3293386".to_string());
+    }
+
+
 }
--- src/posts.rs
+++ src/posts.rs
@@ -1,28 +1,27 @@
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct KeeperData {
-    uid: i64,
+    uid: u64,
 }
 
 
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct RootInterface {
-    id: i64,
+    id: u64,
     created_at: String,
-    uploader_id: i64,
+    uploader_id: u64,
     score: i64,
     source: String,
     #[serde(deserialize_with = "default_if_empty")]
     #[serde(default)]
     md5: String,
     #[serde(deserialize_with = "default_if_empty")]
-    pub last_comment_bumped_at: String,
+    last_comment_bumped_at: String,
     rating: String,
-    image_width: i64,
-    image_height: i64,
+    image_width: u64,
+    image_height: u64,
     tag_string: String,
     is_note_locked: bool,
-    fav_count: i64,
+    fav_count: u64,
     #[serde(deserialize_with = "default_if_empty")]
     #[serde(default)]
     file_ext: String,
@@ -30,15 +29,15 @@ pub struct RootInterface {
     last_noted_at: String,
     is_rating_locked: bool,
     #[serde(deserialize_with = "default_if_empty")]
-    parent_id: i64,
+    parent_id: u64,
     has_children: bool,
     #[serde(deserialize_with = "default_if_empty")]
-    approver_id: i64,
-    tag_count_general: i64,
-    tag_count_artist: i64,
-    tag_count_character: i64,
-    tag_count_copyright: i64,
-    file_size: i64,
+    approver_id: u64,
+    tag_count_general: u64,
+    tag_count_artist: u64,
+    tag_count_character: u64,
+    tag_count_copyright: u64,
+    file_size: u64,
     is_status_locked: bool,
     pool_string: String,
     up_score: i64,
@@ -46,16 +45,18 @@ pub struct RootInterface {
     is_pending: bool,
     is_flagged: bool,
     is_deleted: bool,
-    tag_count: i64,
+    tag_count: u64,
     updated_at: String,
     is_banned: bool,
     #[serde(deserialize_with = "default_if_empty")]
-    pixiv_id: i64,
+    pixiv_id: u64,
     #[serde(deserialize_with = "default_if_empty")]
     last_commented_at: String,
     has_active_children: bool,
-    bit_flags: i64,
-    tag_count_meta: i64,
+    bit_flags: u64,
+    tag_count_meta: u64,
+    #[serde(deserialize_with = "default_if_empty")]
+    #[serde(default)]
     keeper_data: KeeperData,
     uploader_name: String,
     has_large: bool,
@@ -77,6 +78,61 @@ pub struct RootInterface {
     #[serde(default)]
     #[serde(deserialize_with = "default_if_empty")]
     preview_file_url: String,
+}
+
+impl RootInterface {
+
+    pub fn get_large_file_url(&self) -> String {
+        return self.large_file_url.to_string();
+    }
+
+    pub fn get_preview_file_url(&self) -> String {
+        return self.preview_file_url.to_string();
+    }
+
+    pub fn get_image_height(&self) -> u64 {
+        return self.image_height;
+    }
+
+    pub fn get_id(&self) -> u64 {
+        return self.id;
+    }
+
+    pub fn get_file_ext(&self) -> String {
+        return self.file_ext.to_string();
+    }
+
+    pub fn get_image_width(&self) -> u64 {
+        return self.image_width;
+    }
+
+    pub fn get_file_url(&self) -> String {
+        return self.file_url.to_string();
+    }
+
+    pub fn get_last_comment_bumped_at(&self) -> String {
+        return self.last_comment_bumped_at.to_string();
+    }
+
+    pub fn get_tag_string(&self) -> String {
+        return self.tag_string.to_string();
+    }
+
+    pub fn get_tag_string_copyright(&self) -> String {
+        return self.tag_string_copyright.to_string();
+    }
+
+    pub fn get_tag_string_general(&self) -> String {
+        return self.tag_string_general.to_string();
+    }
+
+    pub fn get_tag_string_artist(&self) -> String {
+        return self.tag_string_artist.to_string();
+    }
+
+    pub fn get_tag_string_character(&self) -> String {
+        return self.tag_string_character.to_string();
+    }
 }
 
 fn default_if_empty<'de, D, T>(de: D) -> Result<T, D::Error>
